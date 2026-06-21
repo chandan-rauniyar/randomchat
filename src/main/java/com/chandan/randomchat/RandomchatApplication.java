ba1fa44a55c55e893dc07e3542f1f820fb0559ea
chandan-rauniyar <chandankumar234@lpu.in>
2026-06-21 18:30:31 +0530

    Add startup log message

--- src/main/java/com/chandan/randomchat/RandomchatApplication.java
+++ src/main/java/com/chandan/randomchat/RandomchatApplication.java
@@ -1,13 +1,17 @@
 package com.chandan.randomchat;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
+@Slf4j
 public class RandomchatApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RandomchatApplication.class, args);
+		log.info("RandomChat backend started successfully");
 	}
-
 }
